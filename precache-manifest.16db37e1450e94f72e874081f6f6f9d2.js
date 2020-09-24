142b5a4d996184367ed9eda1f76cc3df63ff3ec8
mohamed <mohamed.boufraine@epitech.eu>
2020-09-24 11:26:36 +0200

    Updates

---
+++ precache-manifest.16db37e1450e94f72e874081f6f6f9d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947b806e908f2a6c264646a78a591db1",
+    "url": "/hacker-news-ReactJS/index.html"
+  },
+  {
+    "revision": "c99d453d7e1d55f5b1f5",
+    "url": "/hacker-news-ReactJS/static/css/main.818257e7.chunk.css"
+  },
+  {
+    "revision": "e54951f389de1a10fcc4",
+    "url": "/hacker-news-ReactJS/static/js/2.d87e4f08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hacker-news-ReactJS/static/js/2.d87e4f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c99d453d7e1d55f5b1f5",
+    "url": "/hacker-news-ReactJS/static/js/main.64a3a3c7.chunk.js"
+  },
+  {
+    "revision": "77cb3dabd20ac227b63d",
+    "url": "/hacker-news-ReactJS/static/js/runtime-main.eda03b7d.js"
+  }
+]);
